1698b28192eb3ee4c09b5339bc17a5bd4001ad28
acmay <hiraeth54@gmail.com>
2020-02-04 00:23:12 -0500

    comp

---
+++ app/src/main/java/com/example/acmay/c196mobileapp/database/StartEntity.java
@@ -0,0 +1,64 @@
+package com.example.acmay.c196mobileapp.database;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity(tableName = "startDates")
+public class StartEntity {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private Date date;
+    private String text;
+
+    @Ignore
+    public StartEntity() {
+    }
+
+    public StartEntity(int id, Date date, String text) {
+        this.id = id;
+        this.date = date;
+        this.text = text;
+    }
+
+    @Ignore
+    public StartEntity(Date date, String text) {
+        this.date = date;
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "TermEntity{" +
+                "id=" + id +
+                ", date=" + date +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
