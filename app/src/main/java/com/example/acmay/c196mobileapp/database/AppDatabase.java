0a0e0e6de79138e16c98f78be72434e1298c89ec
acmay <hiraeth54@gmail.com>
2020-02-10 23:46:12 -0500

    App no longer crashing! Courses do not display, but app is functional.

--- app/src/main/java/com/example/acmay/c196mobileapp/database/AppDatabase.java
+++ app/src/main/java/com/example/acmay/c196mobileapp/database/AppDatabase.java
@@ -1,16 +1,25 @@
 package com.example.acmay.c196mobileapp.database;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.TypeConverters;
+import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
 
 import static android.arch.persistence.room.Room.databaseBuilder;
 
-@Database(entities = {TermEntity.class, CourseEntity.class}, version = 3)
+@Database(entities = {TermEntity.class, CourseEntity.class}, version = 2)
 @TypeConverters(DateConverter.class)
 public abstract class AppDatabase extends RoomDatabase {
+    static final Migration MIGRATION_1_2 = new Migration(1, 2){
+        @Override
+        public void migrate(SupportSQLiteDatabase database){
+            database.execSQL("CREATE TABLE courses(id INTEGER NOT NULL, text TEXT, date INTEGER, PRIMARY KEY (id))");
+        }
+    };
+
     public static final String DATABASE_NAME = "AppDatabase.db";
     private static volatile AppDatabase instance;
     private static final Object LOCK = new Object();
@@ -24,7 +33,7 @@ public abstract class AppDatabase extends RoomDatabase {
             synchronized (LOCK){
                 if(instance == null){
                     instance = Room.databaseBuilder(context.getApplicationContext(),
-                            AppDatabase.class, DATABASE_NAME).build();
+                            AppDatabase.class, DATABASE_NAME).addMigrations(MIGRATION_1_2).build();
                 }
             }
         }
