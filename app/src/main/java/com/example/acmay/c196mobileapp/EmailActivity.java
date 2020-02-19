fdd82298c9dc8e36b2b81cf31c858d40001590ec
acmay <hiraeth54@gmail.com>
2020-02-19 14:15:33 -0500

    Email activity successfuly puts content of note and subject line into email, but still not picking up the address

--- app/src/main/java/com/example/acmay/c196mobileapp/EmailActivity.java
+++ app/src/main/java/com/example/acmay/c196mobileapp/EmailActivity.java
@@ -1,60 +1,124 @@
 package com.example.acmay.c196mobileapp;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.example.acmay.c196mobileapp.database.NoteEntity;
+import com.example.acmay.c196mobileapp.viewmodel.NoteViewModel;
+
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-import static com.example.acmay.c196mobileapp.utilities.Constants.ASS_ID_KEY;
 import static com.example.acmay.c196mobileapp.utilities.Constants.COURSE_ID_KEY;
+import static com.example.acmay.c196mobileapp.utilities.Constants.EDITING_KEY;
 import static com.example.acmay.c196mobileapp.utilities.Constants.EMAIL_ID;
 import static com.example.acmay.c196mobileapp.utilities.Constants.MESSAGE_ID;
+import static com.example.acmay.c196mobileapp.utilities.Constants.NOTE_ID_KEY;
 import static com.example.acmay.c196mobileapp.utilities.Constants.SUBJECT_ID;
 
 public class EmailActivity extends AppCompatActivity {
 
-    @BindView(R.id.recipientTxt)
-    EditText recipTxt;
-    @BindView(R.id.subjectTxt)
-    EditText subTxt;
-    @BindView(R.id.messageTxt)
-    EditText msgTxt;
 
+    @BindView(R.id.recipientTxt)
+    TextView recipTxt;
+    @BindView(R.id.subjectTxt)
+    TextView subTxt;
+    @BindView(R.id.messageTxt)
+    TextView msgTxt;
+
+    //exits Note screen without saving data
     @OnClick(R.id.sendBtn)
-    void sendClickHandler(){
+    void cancelClickHandler(){
+        Log.i("bats", "send: button was pressed");
         String message = msgTxt.getText().toString();
         String recipient = recipTxt.getText().toString();
         String subject = subTxt.getText().toString();
 
         Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.putExtra(EMAIL_ID, recipient);
-        intent.putExtra(SUBJECT_ID, subject);
-        intent.putExtra(MESSAGE_ID, message);
+        intent.putExtra(Intent.EXTRA_EMAIL, recipient);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
         intent.setType("message/rfc822");
 
         startActivity(Intent.createChooser(intent,"Choose Mail App"));
         Log.i("bats", "send: button was pressed");
     }
 
+    private boolean mEditing;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.email_layout);
+        setContentView(R.layout.email_editor);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ButterKnife.bind(this);
+
+        if(savedInstanceState != null){
+            mEditing = savedInstanceState.getBoolean(EDITING_KEY);
+        }
 
         Bundle extras = getIntent().getExtras();
 
-         if(extras != null) {
+        if(extras != null) {
             String note = extras.getString(MESSAGE_ID);
-            //msgTxt.setText(note);
+            msgTxt.setText(note);
         }else if(extras == null){
-             Log.i("email", "onCreate: extras was null");
-         }
+            Log.i("email", "onCreate: extras was null");
+        }
     }
+
+    @Override
+    public void onBackPressed() {
+        saveAndReturn();
+    }
+
+    private void saveAndReturn() {
+        finish();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(EDITING_KEY, true);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //if(!mNewCourse){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        //}
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /*
+        if(item.getItemId() == android.R.id.home){
+            saveAndReturn();
+            return true;
+        } else if(item.getItemId() == R.id.action_delete){
+            mViewModel.deleteCourse();
+            finish();
+        }
+
+         */
+        return super.onOptionsItemSelected(item);
+    }
+
 }
